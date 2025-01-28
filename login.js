6b49b876d7d9271ea624486d794a5f9135c52a65
inelika <dosanenlik@gmail.com>
2025-01-28 19:03:20 +0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+function checkLogin(Login,Password) {
+  const correctLogin = "adam";  
+  const correctPassword = "12345";
+
+  if (Login === correctLogin && Password === correctPassword) {
+      return "You are logged in";
+  } else if (Login !== correctLogin && Password !== correctPassword) {
+      return "Both inputs are wrong";
+  } else if (Login !== correctLogin) {
+      return "Your login is wrong";
+  } else {
+      return "Your password is wrong";
+  }
+}
+
+console.log(checkLogin("adam", "12345")); 
+console.log(checkLogin("user", "12345"));   
+console.log(checkLogin("adam", "09090")); 
+console.log(checkLogin("user", "55555"));  
+console.log(checkLogin ( "enlik", "7777" ))
